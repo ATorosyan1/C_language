488e9b19191daa69522b770879fcdcc1a2684c31
ATorosyan1 <arman_torosyan_1997@mail.ru>
2020-04-03 16:02:27 +0400

    Task

--- projects/tasks/server.c
+++ projects/tasks/server.c
@@ -9,82 +9,12 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-<<<<<<< HEAD
 #include "task.c"
 #include <pthread.h>
-=======
-//#include "task.c"
->>>>>>> 190363d38868941c8aa13c8c9a91a08a6f8ea8fc
 
 #define PORT 8888
 #define ADDRESS "127.0.0.1"
 #define true 1
-<<<<<<< HEAD
-=======
-void in_err(int n,char * message){
-	if(n<0){
-		perror(message);
-		exit(1);
-	}
-}
-int stringConvertToArithmeticOperations(char * buf){
-	
-	int length=0;
-	while(buf[length]!='\0'){
-		length++;
-	}
-	for(int i=0;i<length;i++){
-		if(buf[i]>='A' && buf[i]<='z'){
-			perror("Invalid String!");
-			exit(1);
-		}
-	}
-	char simvols[length];
-	int numbers[length];
->>>>>>> 190363d38868941c8aa13c8c9a91a08a6f8ea8fc
-
-char client_message[1024];
-void * socketThread(void *arg){
-	int newSocket = *((int *)arg);
-	int recv_res=recv(newSocket,client_message,1024,0);	
-	in_err(recv_res,"Error in recv method!");	
-	int resoult=stringConvertToArithmeticOperations(client_message);
-	itoa(resoult,client_message);
-	//sleep(1);
-	int send_res=send(newSocket,client_message,1024,0);
-	in_err(send_res,"Error in send method!");
-	close(newSocket);
-	pthread_exit(NULL);
-}
-<<<<<<< HEAD
-=======
- void reverse(char s[])
- {
-     int i, j;
-     char c;
- 
-     for (i = 0, j = strlen(s)-1; i<j; i++, j--) {
-         c = s[i];
-         s[i] = s[j];
-         s[j] = c;
-     }
- }
-void itoa(int n, char s[])
- {
-     int i, sign;
- 
-     if ((sign = n) < 0)  
-         n = -n;          
-     i = 0;
-     do {       
-         s[i++] = n % 10 + '0';   
-     } while ((n /= 10) > 0);   
-     if (sign < 0)
-         s[i++] = '-';
-     s[i] = '\0';
-     reverse(s);
- }
->>>>>>> 190363d38868941c8aa13c8c9a91a08a6f8ea8fc
 int main(){
 
 	int mainsocket,newsocket,bind_res;
