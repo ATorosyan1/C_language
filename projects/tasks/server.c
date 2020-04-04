29e9bda4d0a4fc916f10c8c8724a6faee4dc99ed
ATorosyan1 <arman_torosyan_1997@mail.ru>
2020-04-04 12:53:37 +0400

    Task

--- projects/tasks/server.c
+++ projects/tasks/server.c
@@ -9,6 +9,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <errno.h>
 #include "task.c"
 #include <pthread.h>
 
@@ -16,18 +17,39 @@
 #define ADDRESS "127.0.0.1"
 #define true 1
 
-char client_message[1024];
+char *client_message;
+void exit_func(void * arg){
+	free(arg);
+	printf("Exit!\n");
+}
 void * socketThread(void *arg){
+	
+	client_message=malloc(1024);
+
+	//prohibits early termination of a thread
+	pthread_setcancelstate(PTHREAD_CANCEL_DISABLE,NULL);
+	
+	//sets the thread completion handler
+	pthread_cleanup_push(exit_func,client_message);
+
 	int newSocket = *((int *)arg);
 	int recv_res=recv(newSocket,client_message,1024,0);	
 	in_err(recv_res,"Error in recv method!");	
 	int resoult=stringConvertToArithmeticOperations(client_message);
 	itoa(resoult,client_message);
-	//sleep(1);
 	int send_res=send(newSocket,client_message,1024,0);
 	in_err(send_res,"Error in send method!");
 	close(newSocket);
-	pthread_exit(NULL);
+
+	//permits early termination of a thread
+	pthread_setcancelstate(PTHREAD_CANCEL_ENABLE,NULL);
+	
+	//creates a thread cancel point.
+	pthread_testcancel();
+
+	//retrieves a thread completion handler
+	pthread_cleanup_pop(1);
+	
 }
 
 int main(){
@@ -49,26 +71,18 @@ int main(){
 	
 	in_err(listen(mainsocket,1),"Error in listen method!");
 	
-	pthread_t tid[30];
-    	int i = 0;
 	while(true){
 		
-		addr_size=sizeof(serverStorage);
-		newsocket=accept(mainsocket,(struct sockaddr *) &serverStorage, &addr_size);
+		pthread_t thread;
+		addr_size = sizeof(serverStorage);
+		newsocket = accept(mainsocket,(struct sockaddr *) &serverStorage, &addr_size);
 		in_err(newsocket,"Error in accept method!");
 
 			
-		if( pthread_create(&tid[i], NULL, socketThread, &newsocket) != 0 )
+		if( pthread_create(&thread, NULL, socketThread, &newsocket) != 0 )
           	printf("Failed to create thread\n");
-		if( i >= 20)
-     		   {
-        		  i = 0;
-         		  while(i < 20)
-      			    {
-        			    pthread_join(tid[i++],NULL);
-        	            }
-         			 i = 0;
-      		    }
+		pthread_cancel(thread);
+		pthread_join(thread,NULL);
 	}
 	close(mainsocket);
 	return 0;
